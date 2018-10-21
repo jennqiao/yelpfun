9bf55378450cde378347687f049f24ff0b1aef1a
Paul Gu <paulgu@Pauls-MacBook-Pro.local>
2018-10-20 21:19:07 -0700

    made a random picker

--- index.js
+++ index.js
@@ -9,21 +9,25 @@ app.use(morgan('dev'));
 
 // app.use(express.static(__dirname));
 
-
-
-
 const apiKey = 'FtVSQA7amcj-me9rNqO1QgHdTk7Vz98JchW16RRcxuD1kZZmrkJTqjN6Y_2trKRiROx1O5g09WYBGgyD6h5VoLwfFi5le5oHdTYr5pYelRpxUDOODJdopp89SevLW3Yx';
 
 const searchRequest = {
-  term:'Four Barrel Coffee',
-  location: 'san francisco, ca'
+  open_now:true,
+  location: 'Redwood City, CA',
+  radius: 15000,
+  limit: 50
 };
 
 const client = yelp.client(apiKey);
 
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+};
+
 client.search(searchRequest).then(response => {
-  const firstResult = response.jsonBody.businesses[0];
-  const prettyJson = JSON.stringify(firstResult, null, 4);
+  let pick = getRandomInt(response.jsonBody.businesses.length)
+  let firstResult = response.jsonBody.businesses[pick];
+  let prettyJson = "Randomly picked ID: " + pick.toString() + "<br><br>" + JSON.stringify(firstResult, null, 4);
   return prettyJson;
 }).catch(e => {
   console.log(e);
@@ -32,8 +36,6 @@ client.search(searchRequest).then(response => {
     res.send(prettyJson);
   });
 });
-
-
 
 app.listen(3000, function() {
   console.log('Listening on port 3000 now');
