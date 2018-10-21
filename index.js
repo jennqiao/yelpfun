36323eb614683504f3a501623b49c34334c3f159
Paul Gu <paulgu@Pauls-MacBook-Pro.local>
2018-10-20 22:18:03 -0700

    ejs

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const yelp = require('yelp-fusion');
+const ejs = require('ejs');
 
 const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs')
+
 app.use(bodyParser.json());
 app.use(morgan('dev'));
+
 
 const apiKey = 'FtVSQA7amcj-me9rNqO1QgHdTk7Vz98JchW16RRcxuD1kZZmrkJTqjN6Y_2trKRiROx1O5g09WYBGgyD6h5VoLwfFi5le5oHdTYr5pYelRpxUDOODJdopp89SevLW3Yx';
 
@@ -22,16 +28,23 @@ function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 };
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+
 	client.search(searchRequest).then(response => {
-	  let pick = getRandomInt(response.jsonBody.businesses.length)
-	  let firstResult = response.jsonBody.businesses[pick];
-	  let prettyJson = "Randomly picked ID: " + pick.toString() + "<br><br>" + JSON.stringify(firstResult, null, 4);
-	  return prettyJson;
+		console.log(response.jsonBody)
+	  return response.jsonBody.businesses;
 	}).catch(e => {
 	  console.log(e);
-	}).then(prettyJson => {
-  	res.send(prettyJson);
+	}).then(results => {
+  	let pick = getRandomInt(results.length)
+	  let firstResult = results[pick];
+
+	  let name = firstResult.name;
+	  console.log(firstResult);
+	  let photo = null;
+
+	  //let prettyJson = JSON.stringify(firstResult, null, 4);
+    res.render('index', {pick: pick, name: name, photo: photo, error: null});
 	});
 });
 
